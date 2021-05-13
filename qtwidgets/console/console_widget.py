e95892b605ce775a263ed07168063417b0702541
flegac <florent.legac@gmail.com>
2021-05-13 12:55:15 +0200

    feat: Console Level filter (work in progress)

--- qtwidgets/console/console_widget.py
+++ qtwidgets/console/console_widget.py
@@ -4,9 +4,10 @@ from typing import List
 
 from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5.QtGui import QContextMenuEvent
-from PyQt5.QtWidgets import QWidget, QPlainTextEdit, QMenu
+from PyQt5.QtWidgets import QWidget, QPlainTextEdit, QMenu, QComboBox
 
 from qtwidgets.console.console_config import ConsoleConfig
+from qtwidgets.observablelist import observablelist
 from qtwidgets.utils import load_ui
 
 
@@ -15,15 +16,47 @@ class LogSignal(QObject):
 
 
 class ConsoleWidget(QWidget):
-    def __init__(self, config: ConsoleConfig=None):
+    def __init__(self, config: ConsoleConfig = None):
         super().__init__()
         config = config or ConsoleConfig('console')
+
+        self.records = observablelist()
+
         self.config = deepcopy(config)
         self.signals = LogSignal()
         self.signals.log.connect(self._write_line)
         self._setup_ui()
         self.handler = QtHandler(self.signals)
         self.set_loggers(config.loggers)
+
+        combo: QComboBox = self.consoleLevel
+        combo.currentIndexChanged.connect(self.change_level)
+
+        self.consoleClearButton.clicked.connect(self.clear)
+
+        self.consoleInput.returnPressed.connect(self.handle_input)
+
+        self.context = dict()
+
+    def handle_input(self):
+        text = self.consoleInput.text()
+        self.consoleInput.clear()
+        self._write_line(f'> {text}')
+        try:
+            self.context = dict(self.context, **globals())
+            exec(text, self.context, self.context)
+        except Exception as e:
+            self._write_line(str(e))
+
+    def change_level(self, i):
+        level = self.consoleLevel.currentText()
+        print(level)
+        self.clear()
+        for record in self.records:
+            if level in record:
+                self._text_box.appendPlainText(record)
+
+        self.handler.setLevel(level)
 
     def set_loggers(self, loggers: List[logging.Logger]):
         for logger in self.config.loggers:
@@ -38,6 +71,7 @@ class ConsoleWidget(QWidget):
     def _write_line(self, log_text: str):
         self._text_box.appendPlainText(log_text)
         self._text_box.centerCursor()
+        self.records.append(log_text)
 
     @property
     def _text_box(self) -> QPlainTextEdit:
