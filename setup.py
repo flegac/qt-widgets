f6ae52db3ccfd955a8251e9853d123b7542c45a6
flegac <florent.legac@gmail.com>
2021-05-17 23:39:27 +0200

    feat: dev5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='qt-widgets',
-    version='0.0.1-dev4',
+    version='0.0.1-dev5',
     author='flegac',
     description='Qt widgets library',
 
